309fe62977737ffeb040603bc4e78c4972994aed
Hanson-HSChang <hanson.hschang@gmail.com>
2024-09-12 13:01:53 -0500

    wip: update type hint

--- homework2/problem4.py
+++ homework2/problem4.py
@@ -1,9 +1,11 @@
 # !sudo apt-get install texlive-full
+from typing import Dict
 import numpy as np
+from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 plt.rcParams.update({"text.usetex": True})
 
-def plot_transformation(matrix_W: np.ndarray, vectors: dict[str, np.ndarray]) -> None:
+def plot_transformation(matrix_W: NDArray, vectors: Dict[str, NDArray]) -> None:
     fig, (ax_original, ax_transformed) = plt.subplots(1, 2, figsize=(12, 5))
     
     # Plot original vectors
@@ -29,13 +31,6 @@ def plot_transformation(matrix_W: np.ndarray, vectors: dict[str, np.ndarray]) ->
     ax_transformed.axvline(x=0, color='k', linestyle='--')
     ax_transformed.grid(True, linestyle=':', alpha=0.7)
 
-    projection_color = 'gray'
-    s = np.linspace(-5, 5, 100)
-    # ax_transformed.plot(matrix_W[0] * s, matrix_W[1] * s, color=projection_color, linestyle="--", alpha=0.5)
-    # ax_transformed.quiver(0, 0, matrix_W[0], matrix_W[1], angles='xy', scale_units='xy', scale=1, color=projection_color, label="$W$")
-
-    # for i, (key, vector) in enumerate(vectors.items()):
-    #     ax_transformed.quiver(0, 0, vector[0], vector[1], angles='xy', scale_units='xy', scale=1, color=f"C{i}", label="$"+key+"$", alpha=0.5)
     for i, (key, vector) in enumerate(vectors.items()):
         
         transformed_v = matrix_W @ vector        
